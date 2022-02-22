596a3a9b25fe16713da3b9b8420b2227c0b438ab
PHB <qls6743@gmail.com>
2022-02-22 19:18:49 +0900

    first commit

--- Trainer/__init__.py
+++ Trainer/__init__.py
@@ -1,5 +1,4 @@
 from Trainer.Basic_trainer import Basic_trainer
-from Trainer.Car_trainer import Car_trainer
 from Trainer.Basic_trainer_with_model import Model_trainer
 
-__all__ = ['Basic_trainer', 'Car_trainer', 'Model_trainer']
+__all__ = ['Basic_trainer', 'Model_trainer']
